dda636a893b5dc769b517869f7155b0a922f2951
peterjozsef <peterjozsef.nagy@edu.bme.hu>
2023-09-27 17:15:19 +0200

    All torpedo fires mode done

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,8 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        firingsucces = primaryTorpedoStore.fire(1);
+        firingsucces = secondaryTorpedoStore.fire(1);
 
         break;
     }
